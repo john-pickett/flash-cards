4efeae47df32b22ef59807b14feb88bb384a9e7a
John P <john.pickett@gmail.com>
2018-04-03 06:05:25 -0500

    component created

---
+++ src/components/hangman/Hangman.vue
@@ -0,0 +1,34 @@
+<template>
+    <v-container grid-list-md>
+    
+        <h2>Hangman</h2>
+        <v-layout row>
+            <v-flex xs-6>
+                <v-card>
+                    <p>hey</p>
+                    <v-card-media :src="require('../../assets/dancing2.jpg')" height="300px"></v-card-media>
+                
+                
+                </v-card>
+            </v-flex>
+            <v-flex xs-6>
+                <v-card>
+                    <p>hey</p>
+                </v-card>
+            </v-flex>
+        </v-layout>
+    </v-container>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style scoped>
+    /* img {
+        display: block;
+        margin: 0 auto;
+    } */
+</style>
